506fe39e4916d1b9f3e198d31b3a17c10607d05d
serhioses <serega.mirosh@gmail.com>
2026-08-04 13:11:17 +0000

    D: add circle to triangle ship

--- player.py
+++ player.py
@@ -17,6 +17,7 @@ class Player(CircleShape):
 
     def draw(self, screen: pygame.Surface) -> None:
         pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
+        pygame.draw.circle(screen, "red", self.position, PLAYER_RADIUS, LINE_WIDTH)
 
     def rotate(self, dt: float) -> None:
         self.rotation += PLAYER_TURN_SPEED * dt

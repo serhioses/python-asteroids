64725339a3382994deeb8edf64937719ab878ac5
serhioses <serega.mirosh@gmail.com>
2026-08-20 20:58:52 +0000

    P: Make the asteroids lumpy instead of perfectly round

---
+++ README.md
@@ -0,0 +1,6 @@
+# Asteroids game on Python with Pygame
+
+## How to play
+Run `curl -LsSf https://astral.sh/uv/install.sh | sh` to install `uv`.
+
+Run the game `uv run main.py`.
--- asteroid.py
+++ asteroid.py
@@ -7,18 +7,27 @@ from circleshape import CircleShape
 class Asteroid(CircleShape):
     def __init__(self, x: float, y: float, radius: float) -> None:
         super().__init__(x, y, radius)
+        points_num = random.randint(8, 12)
+        angle_step = 360 / points_num
+        self.__points: list[pygame.Vector2] = []
+
+        for i in range(points_num):
+            angle = i * angle_step
+            point = pygame.Vector2(0, random.uniform(radius * 0.8, radius * 1.2)).rotate(angle)
+            self.__points.append(point)
 
     def draw(self, screen: pygame.Surface) -> None:
-        pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+        points = list(map(lambda p: p + self.position, self.__points))
+        pygame.draw.polygon(screen, "white", points, LINE_WIDTH)
 
     def update(self, dt: float) -> None:
         self.position += self.velocity * dt
         if self.position.x - self.radius > SCREEN_WIDTH:
             self.position.x = -self.radius
-        elif self.position.y - self.radius > SCREEN_HEIGHT:
-            self.position.y = -self.radius
         elif self.position.x + self.radius < 0:
             self.position.x = SCREEN_WIDTH + self.radius
+        if self.position.y - self.radius > SCREEN_HEIGHT:
+            self.position.y = -self.radius
         elif self.position.y + self.radius < 0:
             self.position.y = SCREEN_HEIGHT + self.radius
 
@@ -35,4 +44,3 @@ class Asteroid(CircleShape):
         ast2 = Asteroid(self.position.x, self.position.y, new_radius)
         ast1.velocity = ast1_velocity * 1.2
         ast2.velocity = ast2_velocity * 1.2
-
